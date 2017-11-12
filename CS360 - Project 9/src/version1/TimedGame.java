3f4d6a20946f6dcc0cdee8621780e5ee70fdb9c3
DSladeB <davidsbell94@gmail.com>
2017-11-11 23:58:46 -0500

    Added TimedGame (and tests if it committing works)

---
+++ CS360 - Project 9/src/version1/TimedGame.java
@@ -0,0 +1,15 @@
+package version1;
+
+public class TimedGame 
+{
+	GameBoard gameBoard;
+	Queue queue;
+	//TODO Time Variable;
+	
+	public TimedGame()
+	{
+		gameBoard = new GameBoard();
+		queue = new Queue();
+		//TODO: Initialize the time variable
+	}
+}
