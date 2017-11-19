fe253c3721f98806596d6f48b7c7cc27ba3a0432
Alex <Alex@TheFoxsDen>
2017-11-19 11:37:40 -0500

    Updated high score method.

--- CS360 - Project 9/src/version1/HighScoreTesting.java
+++ CS360 - Project 9/src/version1/HighScoreTesting.java
@@ -4,8 +4,8 @@ public class HighScoreTesting {
 
 	public static void main(String[] args) throws IOException{
 		High_Scores highScores = new High_Scores();
-		int[] numbers = {55, 23, 41, 67, 80, 94, 100, 22, 3, 47};
-		String[] strings = {"Alex", "Brittney","Cindy", "Daniel", "Evan","Francesca", "Gil", "Hilary", "Ike", "Julie"};
+		int[] numbers = {55, 23, 41, 67, 80, 94, 100, 22, 3, 47, 120, 150, 133, 123, 144};
+		String[] strings = {"Alex", "Brittney","Cindy", "Daniel", "Evan","Francesca", "Gil", "Hilary", "Ike", "Julie", "Kate", "Luke", "Michelle", "Nick", "Olivia"};
 		/*for(int i = 0; i< numbers.length; i++){
 			highScores.addScore(strings[i], numbers[i]);
 		}
--- CS360 - Project 9/src/version1/High_Scores.java
+++ CS360 - Project 9/src/version1/High_Scores.java
@@ -13,10 +13,11 @@ public class High_Scores {
 	private String[] names;
 	private int[] scores;
 
-	public High_Scores(){
+	public High_Scores() {
 		names = new String[10];
 		scores = new int[10];
 	}
+
 	public void addScore(String name, int score) throws IOException {
 		FileWriter fwriter = new FileWriter("HighScores.txt", true);
 		PrintWriter outputFile = new PrintWriter(fwriter);
@@ -72,35 +73,58 @@ public class High_Scores {
 						+ "8th Place:\t" + names[7] + "\t" + scores[7] + "\n" + "9th Place:\t" + names[8] + "\t"
 						+ scores[8] + "\n" + "10th Place:\t" + names[9] + "\t" + scores[9]);
 	}
-	public void testScores(String[] namesTest, int[] scoresTest){
+
+	public void testScores(String[] namesTest, int[] scoresTest) {
 		String tempName = "";
 		int tempScore = 0;
 		int temp = 0;
 		String temp2 = "";
 		int counter = 0;
-		while(counter < scoresTest.length){
-			if(counter < 10){
-				scores[counter] = scoresTest[counter];
-				names[counter] = namesTest[counter];
-			}
-			else{
-				tempScore = scoresTest[counter];
-				tempName = namesTest[counter];
-				for (int i = 0; i < scores.length; i++) {
-					if (tempScore > scores[i]) {
-						for (int j = i; j < scores.length - 1; j++) {
-							temp = scores[j];
-							temp2 = names[j];
-							scores[j] = tempScore;
-							names[j] = tempName;
-							scores[j + 1] = temp;
-							names[j + 1] = temp2;
-						}
-
-					}
-				}
-			}
+		int counter2 = 0;
+		boolean nextNull = false;
+		while (counter < 10) {
+			scores[counter] = scoresTest[counter];
+			names[counter] = namesTest[counter];
 			counter++;
 		}
+		for (int i = 0; i < scores.length; i++) {
+			for (int j = 1; j < scores.length - i; j++) {
+				if (scores[j - 1] > scores[j]) {
+					temp = scores[j - 1];
+					scores[j - 1] = scores[j];
+					scores[j] = temp;
+					tempName = names[j - 1];
+					names[j - 1] = names[j];
+					names[j] = tempName;
+				}
+			}
+		}
+		counter = 10;
+		while (counter < scoresTest.length) {
+			tempScore = scoresTest[counter];
+			tempName = namesTest[counter];
+			for (int i = 0; i < scores.length; i++) {
+				if (tempScore > scores[i]) {
+					for (int j = i; j < scores.length - 1; j++) {
+						scores[j+1] = scores[j];
+						scores[j] = tempScore;
+						names[j+1] = names[j];
+						names[j] = tempName;
+						/*tempScore = scores[j];
+						temp = names[j];
+						scores[j] = tempScore;
+						names[j] = tempName;
+						scores[j + 1] = temp;
+						names[j + 1] = temp2;*/
+					}
+
+				}
+			}
+
+			counter++;
+		}
+	
+
+		
 	}
 }
